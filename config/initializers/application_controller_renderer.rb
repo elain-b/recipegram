dc61a672a32b9ab25cefa836694a57bd0dca2991
bern <51830002.cj@gmail.com>
2021-03-08 22:22:37 +0900

    make it better

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '96ce875179401056e8f04d52c8bb186a4a169f4c0b4af48bb0e88d446da44c7a8abbad9b3a3299b1f607a313b655ae365d636c879c8e52d5a0655b3f662527ab'
